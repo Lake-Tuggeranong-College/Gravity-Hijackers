4259dfddd31fe4b6d2351ce118acd7370cadb985
Fyrope83 <0502741@schoolsnet.act.edu.au>
2026-03-11 12:04:02 +1100

    video settings(mostly) completed

--- Settings/tabs/Scripts/window.gd
+++ Settings/tabs/Scripts/window.gd
@@ -3,8 +3,32 @@ extends Control
 @onready var option_button: OptionButton = $HBoxContainer/OptionButton as OptionButton
 
 const WINDOW_MODE_ARRAY : Array[String] = [
-	"Full_screen",
+	"Fullscreen",
 	"Window Mode",
 	"Borderless Window",
 	"Borderless Fullscreen",
 ]
+
+
+func _ready():
+	add_window_mode_items()
+	option_button.item_selected.connect(on_window_mode_selected)
+
+func add_window_mode_items():
+	for window_mode in WINDOW_MODE_ARRAY:
+		option_button.add_item(window_mode)
+
+func on_window_mode_selected(index : int):
+	match index:
+		0: #Fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS,false)
+		1: #Window Mode
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS,false)
+		2: #Borderless Window
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS,true)
+		3: #Borderless Fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS,true)
