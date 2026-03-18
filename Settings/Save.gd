9f8a2da275ea14fd78b70a4911dee3c106189153
Fyrope83 <0502741@schoolsnet.act.edu.au>
2026-03-18 12:05:16 +1100

    Save function stated and FOV

--- Settings/Save.gd
+++ Settings/Save.gd
@@ -1,3 +1,35 @@
 extends Node
 
-const SAVEFILE = ""
+const SAVEFILE = "user://Settings.save"
+
+var game_data = {}
+
+func _ready():
+	load_data()
+
+func load_data():
+	var file = FileAccess.new()
+	if not file.file_exists(SAVEFILE):
+		game_data = {
+			"Window_mode": "Fullscreen",
+			"Vsync_on": false,
+			"Display_fps": false,
+			"Max_fps": 0,
+			"Brightness": 1,
+			"Master_vol": 10,
+			"Music_vol": 10,
+			"SFX_vol": 10,
+			"FOV": 70,
+			"Mouse_sens": .1,
+		}
+		save_data()
+	file.open(SAVEFILE, FileAccess.READ)
+	game_data = file.get_var()
+	file.close()
+
+func save_data():
+	var file = FileAccess.new()
+	file.open(SAVEFILE, file.WRITE)
+	file.store_var(game_data)
+	file.close()
+	
